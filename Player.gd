26a5693e7cb1b0e1c42f57df2bcf0232740417a0
Chris <azraellestudio@gmail.com>
2020-05-01 12:03:48 +0100

    Git test
    About halfway through testing source control

---
+++ Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+var score : int = 0
+var speed : int = 200
+var jumpForce : int = 600
+var gravity : int = 800
+var velocity : Vector2 = Vector2()
+onready var sprite : Sprite = get_node("Sprite")
+ 
+func _physics_process(delta):
+	velocity.x = 0
+	
+	#movement input
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed
+	
+	#apply movvement
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	#gravity
+	velocity.y += gravity * delta
+	
+	#jump input
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y -= jumpForce
+	
+	#sprite direction
+	if velocity.x < 0:
+		sprite.flip_h = true
+	elif velocity.x > 0:
+		sprite.flip_h = false
+
+func die():
+# warning-ignore:return_value_discarded
+	get_tree().reload_current_scene()
